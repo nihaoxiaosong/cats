3d5855e1e8796c93f5c8f08ba2151ed564461678
songjie <s363059330@qq.com>
2016-12-01 17:20:01 +0800

    20161201

---
+++ src/main/java/com/hx/entity/base/Pager.java
@@ -0,0 +1,81 @@
+package com.hx.entity.base;
+
+import java.util.List;
+
+public class Pager<T> {
+	/**
+	 * 当前页数
+	 */
+	private int currentPage;
+	/**
+	 * 每页条数
+	 */
+	private int pageSize;
+	/**
+	 * 从第几条开始
+	 */
+	private int start;
+	/**
+	 * 总记录数。
+	 */
+	private int totalCount;
+
+	/**
+	 * 当前页的记录列表
+	 */
+	private List<T> list;
+	
+	public Pager() {
+		super();
+	}
+
+	public Pager(int currentPage, int pageSize, int start, int totalCount, List<T> list) {
+		super();
+		this.currentPage = currentPage;
+		this.pageSize = pageSize;
+		this.start = (currentPage-1)*pageSize;
+		this.totalCount = totalCount;
+		this.list = list;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public List<T> getList() {
+		return list;
+	}
+
+	public void setList(List<T> list) {
+		this.list = list;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/pages/Common/pager.ftl
@@ -0,0 +1,12 @@
+<div style="float: right;" id="pager">
+	<nav>
+		<ul class="pagination">
+			<li><a href="/cats/attendance/info?page=1">首页</a></li>
+			<li class="disabled"><a href="#">上一页</a></li>
+			<li class="active"><a href="#">1</a></li>
+			<li><a href="/cats/attendance/info?page=2">2</a></li>
+			<li><a href="/cats/attendance/info?page=2">下一页</a></li>
+			<li><a href="/cats/attendance/info?page=2">尾页</a></li>
+		</ul>
+	</nav>
+</div>
